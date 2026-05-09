ec1d781e48969fa861e3773aa7f57cad9029e520
kavali5096-pixel <kavali5096@gmail.com>
2026-05-09 14:31:26 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,3 +9,9 @@ def buildArtifact()
     sh 'mvn package' 
     
 }
+
+def deployTomcat(jobname,ip,context)
+{
+   sh 'scp /var/lib/jenkins/workspace/${jobname/webapp/target/webapp.war  ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war
+
+}
